afdd2bf7b25e138a621aa18f154d1e6c362dc651
Matthew Doles <matteddied@gmail.com>
2022-01-11 17:42:42 -0500

    Init commit & navigation component

---
+++ src/components/Nav.svelte
@@ -0,0 +1,9 @@
+<div class="navbar mb-5 shadow-md bg-purple-600 text-neutral-content">
+	<div class="px-2 mx-2">
+		<span class="text-lg font-bold">Good Word Hunting</span>
+	</div>
+	<div class="px-2 mx-2">
+		<a class="btn btn-ghost btn-sm rounded-btn" href="/">Home</a>
+		<a class="btn btn-ghost btn-sm rounded-btn" href="/about">About</a>
+	</div>
+</div>
