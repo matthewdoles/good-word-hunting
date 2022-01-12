441f67b8f04010ce96192d9002b9513ab5810663
Matthew Doles <matteddied@gmail.com>
2022-01-12 09:53:11 -0500

    Credits endpoint & Card component

---
+++ src/components/Card.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	import { loop_guard } from 'svelte/internal';
+	import { scale } from 'svelte/transition';
+	export let word;
+
+	const dispatch = createEventDispatcher();
+
+	const cardClasses =
+		'p-6 bg-gray-100 text-gray-800 text-center ' +
+		'rounded-md shadow-sm hover:shadow-md flex flex-col items-center ' +
+		'dark:bg-gray-700 dark:hover:shadow-[0px_0px_4px_rgba(255,255,255,0.7)]';
+</script>
+
+<div class={cardClasses} in:scale on:click={() => dispatch('cardselected', { word })}>
+	<h2 class="capitalize font-bold dark:text-white">
+		{word}
+	</h2>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,21 +1,43 @@
 <script>
 	import { onMount } from 'svelte';
 	import { dev } from '$app/env';
+	import Card from '../components/Card.svelte';
 
 	let movie;
 	let keywords = [];
+	let cast = [];
 
 	onMount(async () => {
 		const baseUrl = dev ? import.meta.env.VITE_DEV_API : import.meta.env.VITE_PROD_API;
 		const movieResponse = await fetch(baseUrl + '/api/random-movie');
 		movie = await movieResponse.json();
 		const keywordsResponse = await fetch(baseUrl + `/api/${movie.id}-keywords`);
-		keywords = await keywordsResponse.json();
+		const allKeywords = await keywordsResponse.json();
+		allKeywords.forEach((keyword, i) => {
+			if (i < 10) {
+				keywords = [...keywords, keyword];
+			}
+		});
+		const castResponse = await fetch(baseUrl + `/api/${movie.id}-credits`);
+		const allCast = await castResponse.json();
+		allCast.forEach((castMember, i) => {
+			if (i < 10) {
+				cast = [...cast, castMember];
+			}
+		});
+		console.log(allCast);
 	});
 </script>
 
-<h3 class="text-2xl dark:text-white">5 words used to describe this movie are...</h3>
-{#each keywords as keyword}
-	<p>{keyword.name}</p>
-{/each}
+<h3 class="text-2xl dark:text-white">{keywords.length} words used to describe this movie are...</h3>
+<div class="my-4 grid gap-4 md:grid-cols-4 grid-cols-2">
+	{#each keywords as keyword}
+		<Card word={keyword.name} />
+	{/each}
+</div>
 <h3 class="text-2xl dark:text-white">How many names do you need to guess this movie?</h3>
+<div class="my-4 grid gap-4 md:grid-cols-4 grid-cols-2">
+	{#each cast as credit, i}
+		<Card word={i + 1} on:cardselected={(event) => console.log(event.detail.word)} />
+	{/each}
+</div>
