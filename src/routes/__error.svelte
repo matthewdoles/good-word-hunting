3c743309a450ec4a95998a1ad6a33d20c2638311
Matthew Doles <matteddied@gmail.com>
2022-01-30 15:03:22 -0500

    Handling invalid routes

---
+++ src/routes/__error.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { goto } from '$app/navigation';
+  import Modal from '../components/Modal.svelte';
+
+  let showModal = true;
+
+  const handleCloseModal = () => {
+    showModal = false;
+    goto(`/`);
+  };
+</script>
+
+{#if showModal}
+  <input class="modal-toggle" type="checkbox" bind:checked={showModal} />
+  <Modal message="Sorry! Invalid page." on:closemodal={handleCloseModal} />
+{/if}
